325a5f2be654f41eab2a9240e315cc129acfa202
PrinceNarteh <prinarteh@gmail.com>
2021-09-11 15:43:32 +0000

    Add UsersRespository

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
   providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { CreateUserInput } from './dto/create-user.input';
 import { User } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User) private usersRepository: Repository<User>,
-  ) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async findAll(): Promise<User[]> {
     return await this.usersRepository.find();
