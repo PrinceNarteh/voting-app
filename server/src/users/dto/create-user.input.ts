2ee07de0596dcaa2dd3c3a0001a4a77241d8df0d
PrinceNarteh <prinarteh@gmail.com>
2021-09-11 09:28:43 +0000

    Add Validation

--- server/src/users/dto/create-user.input.ts
+++ server/src/users/dto/create-user.input.ts
@@ -1,13 +1,19 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @MinLength(3, { message: 'Username must have at least 3 characters' })
+  @IsString()
   @Field()
   username: string;
 
+  @IsEmail()
   @Field()
   email: string;
 
+  @MinLength(6, { message: 'Password should be at least six(6) characters.' })
+  @IsString()
   @Field()
   password: string;
 }
