de933aa14a2d88f7a7fbb052a96ddbc2aa211525
PrinceNarteh <prinarteh@gmail.com>
2021-09-11 08:23:52 +0000

    Add TypeORM and ConfigModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
-
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: join(process.cwd(), "src/schema.gql")
-  }), UsersModule],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT) || 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
