06c8ff4d2a0f74f23a4caaa205c5e453b1b459bf
PrinceNarteh <prinarteh@gmail.com>
2021-09-12 05:50:08 +0000

    Hash User's Password

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { randomBytes, scrypt as _script } from 'crypto';
+
 import { ErrorResponse } from 'src/shared/error.response';
+import { promisify } from 'util';
 import { SignUpInput } from './dto/signup.input';
 import { User } from './entities/user.entity';
 import { UsersRepository } from './users.repository';
+
+const scrypt = promisify(_script);
 
 @Injectable()
 export class UsersService {
@@ -18,7 +23,8 @@ export class UsersService {
 
   async create(signUpInput: SignUpInput): Promise<ErrorResponse[] | null> {
     const { email, password } = signUpInput;
-    const userExist = this.usersRepository.findOne({ where: { email } });
+    const userExist = await this.usersRepository.findOne({ where: { email } });
+    console.log(userExist);
     if (userExist) {
       return [
         {
@@ -27,7 +33,15 @@ export class UsersService {
         },
       ];
     }
-    const user = this.usersRepository.create(signUpInput);
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const hashedPassword = salt + '.' + hash.toString('hex');
+
+    const user = this.usersRepository.create({
+      ...signUpInput,
+      password: hashedPassword,
+    });
     await this.usersRepository.save(user);
     return null;
   }
